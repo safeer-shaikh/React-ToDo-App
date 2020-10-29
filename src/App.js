29d3a46007cc25c3334422005092954fa065674f
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-10-29 15:12:28 +0500

    added adding todo items feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import './App.css'
+class App extends 
+React.Component{
+  constructor(){
+    super()
+    this.state = {
+      todos: ['safeer','waleed'],
+      value: '',
+    }
+  }
+  add_todo = () => {
+    this.state.todos.push(this.state.value)
+    this.setState({
+      todos: this.state.todos,
+      value: '',
+    })
+  }
+  render(){
+    return(
+      <>
+        <div className='container' >
+          <h1>To Do Application</h1>
+          <div>
+            <input value={this.state.value} onChange={(e)=>this.setState({value: e.target.value})} type='text' placeholder='Enter ToDo'/>
+            <button onClick={this.add_todo} >Add</button>
+            <ul>
+              {
+                this.state.todos.map((v,i)=>(
+                  <li key={i} >{v}</li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
-
 export default App;
